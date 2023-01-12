707f5a5467a225b3da4d1e1b7975d6d9222bad1a
Satyam Joshi <66298565+Satyam-3@users.noreply.github.com>
2023-01-12 16:42:27 +0530

    taing input

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var username = prompt("Give me your username")
-var welcomeMessage = "this script works " + username
-alert(welcomeMessage)
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+
+function clickHandler(){
+    console.log("clicked!");
+    console.log("input", txtInput.value)
+
+}
+
+btnTranslate.addEventListener("click", clickHandler)
+
