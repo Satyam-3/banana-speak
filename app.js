55c47f727acb372b5f21709f10305664993fbdfc
Satyam Joshi <66298565+Satyam-3@users.noreply.github.com>
2023-01-21 18:56:24 +0530

    app done

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
+var txtInput = document.querySelector("#text-area-input");
+var outputDiv = document.querySelector("#text-area-output");
 
-function clickHandler(){
-    console.log("clicked!");
-    console.log("input", txtInput.value)
+var serverURL = "https://api.funtranslations.com/translate/navi.json"
 
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
 }
 
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something went wrong with server! please try again after some time")
+}
+function clickHandler(){
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then (response => response.json())
+    .then (json =>{
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; 
+    })
+    .catch(errorHandler)
+};
+
 btnTranslate.addEventListener("click", clickHandler)
+
 
